29900acf6896f907a2d713b455236172b67259da
Farhan Yousaf <58497319+farhanyousaf786@users.noreply.github.com>
2023-03-21 11:26:23 -0500

    done

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import userRoutes from "./routes/users.js";
 import dotenv from "dotenv";
+import path from "path";
+import logger from 'morgan';
 
-dotenv.config();
 const app = express();
-const PORT = 5000 || process.env.PORT;
 
+
+
+
+const __dirname = path.resolve();
+dotenv.config();
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
 app.use(cors());
-app.use("/", userRoutes);
 
+
+
+
+
+
+app.use("/", userRoutes);
 app.get("/", (req, res) => res.send("Hello from Express"));
 app.all("*", (req, res) => res.send("route does not exsits"));
 
@@ -26,6 +38,9 @@ app.all("*", (req, res) => res.send("route does not exsits"));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
+
+const PORT = 5000 || process.env.PORT;
 
 //listen server
 app.listen(PORT, () => {
