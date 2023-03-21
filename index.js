070d89a5f06d20f3b3f9f23a56623705747e7560
Farhan Yousaf <58497319+farhanyousaf786@users.noreply.github.com>
2023-03-21 12:55:49 -0500

    added styling

--- index.js
+++ index.js
@@ -4,35 +4,25 @@ import cors from "cors";
 import userRoutes from "./routes/users.js";
 import dotenv from "dotenv";
 import path from "path";
-import logger from 'morgan';
+import logger from "morgan";
 
 const app = express();
 
-
-
-
 const __dirname = path.resolve();
 dotenv.config();
-app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "build")));
 app.use(bodyParser.json());
 app.use(cors());
-
-
-
-
-
 
 app.use("/", userRoutes);
 app.get("/", (req, res) => res.send("Hello from Express"));
 app.all("*", (req, res) => res.send("route does not exsits"));
 
-
 // "catch all" route
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-
 
 const PORT = 5000 || process.env.PORT;
 
