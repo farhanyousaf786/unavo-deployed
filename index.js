73422be9c87d602515996c205c9df38a9e65c8c7
Farhan Yousaf <58497319+farhanyousaf786@users.noreply.github.com>
2023-03-21 11:15:56 -0500

    done

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import userRoutes from "./routes/users.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-const port = 5000;
+const PORT = 5000 || process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -13,9 +15,19 @@ app.use("/", userRoutes);
 app.get("/", (req, res) => res.send("Hello from Express"));
 app.all("*", (req, res) => res.send("route does not exsits"));
 
+//static files
+// app.use(express.static(path.join(__dirname, "./build")));
+
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./build/index.html"));
+// });
+
 // "catch all" route
-app.get("*", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(port, () => console.log("server is listing port 5000"));
+//listen server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
