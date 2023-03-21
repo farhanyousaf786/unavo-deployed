5ff4856b0d6e634a89363823d2c20a79e3751b0e
Farhan Yousaf <58497319+farhanyousaf786@users.noreply.github.com>
2023-03-21 10:52:28 -0500

    done

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
 import cors from "cors";
 import userRoutes from "./routes/users.js";
 
 const app = express();
 const port = 5000;
 
-
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/", userRoutes);
 
-
-
-app.get("/",   (req, res)=> res.send("Hello from Express"));
-app.all("*", (req, res)=> res.send("route does not exsits"));
-
+app.get("/", (req, res) => res.send("Hello from Express"));
+app.all("*", (req, res) => res.send("route does not exsits"));
 
 // "catch all" route
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
-  
-app.listen(port, ()=> console.log('server is listing port 5000'))
- 
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(port, () => console.log("server is listing port 5000"));
